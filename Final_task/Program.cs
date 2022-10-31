b33cb0e56ecfde484a14d8c08f05df9d15280050
KnyazevichT <tknyazevich86@mail.ru>
2022-10-31 13:04:21 +0300

    функция вывода массива

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -3,42 +3,39 @@ Console.Write("Сколько элементов Вы хотите ввести?
 
 int value = Convert.ToInt32(Console.ReadLine());
 
-string[] arrayStrings = new string[value];
+string[] AskArray = new string[value];
 for (int i = 0; i < value; i++)
 {
     Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = Convert.ToString(Console.ReadLine());
-    arrayStrings[i] = element;
+    string res = Convert.ToString(Console.ReadLine());
+    AskArray[i] = res;
 }
 
-string[] arrayFinal = new string[value];
+string[] TotalArray = new string[value];
 int len = 3;
-int pos = 0;
+int n = 0;
 
 for (int j = 0; j < value; j++)
 {
-    if (arrayStrings[j].Length <= len)
+    if (AskArray[j].Length <= len)
     {
-        arrayFinal[pos] = arrayStrings[j];
-        pos++;
+        TotalArray[n] = AskArray[j];
+        n++;
     }
 }
 
 // функция вывода массива
+
 string PrintArray(string[] array)
 {
     string result = string.Empty;
-    result = "[ ";
     for (int i = 0; i < array.Length; i++)
     {
-       Console.Write(array[i] + " ");
-        result += $"{array[i],1}";
-       if (i < array.Length - 1) result += ", ";
+    Console.Write(array[i] + ", ");
     }
 
-    result += " ]";
     return result;
 }
 
 Console.WriteLine("элементы массива, длина которых меньше либо равна 3 символа:");
-Console.WriteLine($"{PrintArray(arrayFinal)}");
+Console.WriteLine($"{PrintArray(TotalArray)}");
